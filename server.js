4ed5a44209df7121358f29c7e28f9e23e7c7ec76
Keiran <keza@keza27.(none)>
2011-08-28 16:25:21 +1000

    Added main page and static file serving functionality.

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var field = require('./gof.js'),
-		map 	= field.Map([100, 100]);
+var express = require('express');
+var app     = express.createServer();
+
+var field   = require('./gof.js');
+var	map 	  = field.Map([100, 100]);
 setInterval(function() {
 	map.update();
 }, 2000);
 
-var http = require('http');
-http.createServer(function (req, res) {
+app.configure(function () {
+  app.use(express.static(__dirname + '/gof'));
+});
+//app.register('.html', require('ejs'));
+app.set("view options", { layout: false }) 
+
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+app.get('/data', function(req, res){
 	console.log("request")
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(JSON.stringify(map));
-}).listen(8080);
+});
+
+app.listen(8080);
 console.log('game of life server running at http://127.0.0.1:8080/');
