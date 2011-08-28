cc7b8ed7d4f1ec7f3756eae171ca0f1e6cf99c67
Andrew Newman <andrewfnewman@gmail.com>
2011-08-28 16:04:02 +1000

    Cleanup

---
+++ test/all.js
@@ -0,0 +1,12 @@
+// browser env
+exports.testGamejs = require('./gamejs');
+exports.testSprite = require('./gamejs/sprite');
+exports.testMask = require('./gamejs/mask');
+
+exports.testVector = require('./gamejs/utils/vectors');
+exports.testAstar = require('./gamejs/pathfinding');
+exports.testBinaryHeap = require('./gamejs/utils/binaryheap');
+// start the test runner if we're called directly from command line
+if (require.main == module.id) {
+    require('test').run(exports);
+}
